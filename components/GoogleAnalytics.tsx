e7b4343d8d3dcbb12c0f2c769a8648bd2c335b6b
komekome898-web <komekome898@gmail.com>
2026-08-25 21:36:13 +0900

    feat: add GA4 component

---
+++ components/GoogleAnalytics.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import Script from 'next/script';
+
+const measurementId = 'G-B9Q283QVER';
+
+export function GoogleAnalytics() {
+  if (process.env.NODE_ENV !== 'production') return null;
+
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
+        strategy="afterInteractive"
+      />
+      <Script id="ga4-init" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+window.gtag = gtag;
+gtag('js', new Date());
+gtag('config', '${measurementId}');`}
+      </Script>
+    </>
+  );
+}
